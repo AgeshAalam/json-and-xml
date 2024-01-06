3a2b3ac558d7f98276c676d48cb3522e5fd19134
AgeshAalam <44727659+AgeshAalam@users.noreply.github.com>
2024-01-06 15:37:54 +0530

    date endpoint added (#1)

--- src/main/java/com/example/jsonandxml/TestController.java
+++ src/main/java/com/example/jsonandxml/TestController.java
@@ -1,8 +1,17 @@
 package com.example.jsonandxml;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
 
 @RestController
 public class TestController {
+
+    @GetMapping("/api/test")
+    public LocalDate getTime(){
+        return LocalDate.now();
+    }
 
 }
