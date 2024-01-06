ccca7321386ba212cecbdcc15a969685852e1acd
AgeshAalam <44727659+AgeshAalam@users.noreply.github.com>
2024-01-06 15:26:16 +0530

    json to xml

---
+++ src/main/java/com/example/jsonandxml/JsonAndXmlApplication.java
@@ -0,0 +1,15 @@
+package com.example.jsonandxml;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class JsonAndXmlApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(JsonAndXmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsonandxml/JsonAndXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonandxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonAndXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
